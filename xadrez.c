e16b5fdd9044da42aec7ebe9baecfee340d58919
unknown <samuelcordeirodasilva1@gmail.com>
2026-01-29 19:51:35 -0300

    quarto commit

--- xadrez.c
+++ xadrez.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
+
 //PROGRAMA: SIMULAÇÃO DE MOVIMENTAÇÃO DE PEÇAS DE XADREZ
-//OBJETIVO: DEMONSTRAR O USO DE LOOPS (WHILE, DO-WHILE E FOR)
+//OBJETIVO: Demonstrar o uso de loops (for, while, do-while) e loops aninhados.
+
 
 int main() {
-    // DEFININDO AS CPNSTANTES PARA O NÚMERO DE CASAS
+    // DEFINIÇÃO DAS CONSTANTES (INTEIROS)
     const int MOVIMENTO_TORRE = 5;
     const int MOVIMENTO_BISPO = 5;
     const int MOVIMENTO_RAINHA = 8;
-
-    // --- MOVIMENTO DA TORRE ---
     
+    // CONSTANTES PARA O CAVALO
+    const int CAVALO_VERTICAL = 2;
+    const int CAVALO_HORIZONTAL = 1;
+
+    // --- MOVIMENTO DA TORRE (LOOP FOR) ---
     printf("Movimento da Torre:\n");
     for (int i = 1; i <= MOVIMENTO_TORRE; i++) {
         printf("Direita\n");
     }
     printf("\n");
 
-    // --- MOVIMENTO DO BISPO ---
-    
+    // --- MOVIMENTO DO BISPO (LOOP WHILE) ---
     printf("Movimento do Bispo:\n");
     int contadorBispo = 1;
     while (contadorBispo <= MOVIMENTO_BISPO) {
@@ -27,21 +31,35 @@ int main() {
     }
     printf("\n");
 
-    // --- MOVIMENTO DA RAINHA ---
-    
+    // --- MOVIMENTO DA RAINHA (LOOP DO-WHILE) ---
     printf("Movimento da Rainha:\n");
     int contadorRainha = 1;
     do {
         printf("Esquerda\n");
         contadorRainha++;
     } while (contadorRainha <= MOVIMENTO_RAINHA);
+    printf("\n");
+
+    // --- MOVIMENTO DO CAVALO (LOOPS ANINHADOS) ---
+    
+    printf("Movimento do Cavalo:\n");
+
+    // LOOP EXTERNO 'FOR' CONTROLA A EXECUÇÃO DA MANOBRA EM L
+    for (int i = 0; i < 1; i++) {
+        int passosBaixo = 1;
+        
+        // LOOP INTERNO 'WHILE' EXECUTA AS 2 CASAS PARA BAIXO
+        while (passosBaixo <= CAVALO_VERTICAL) {
+            printf("Baixo\n");
+            passosBaixo++;
+        }
+        
+        // FINALIZAÇÃO MOVIMENTO 'L'
+        printf("Esquerda\n");
+    }
 
     return 0;
 }
-
-
-
-
 
 
 
